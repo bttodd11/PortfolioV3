deb0087bdbf9f93d8204df9a7520fb140576fc72
bttodd11 <bttodd@icloud.com>
2019-05-05 16:46:32 -0500

    Updated jWuery

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,4 +1,5 @@
 
 $(document).ready(function(){
-$(".port").animate({opacity:.7},2000).animate({fontSize:"54px"}).animate({letterSpacing:"25px"})
+$(".port").animate({opacity:.3},2000).animate({fontSize:"60px"}).animate({letterSpacing:"10px"})
 });
+
