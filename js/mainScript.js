3eada15946d3fa4c85902840597dbc9032fb9275
bttodd11 <bttodd@icloud.com>
2019-05-08 11:43:11 -0500

    Updated CSS

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function(){
-$(".port").animate({opacity:.3},2000).animate({fontSize:"60px"}).animate({letterSpacing:"10px"})
+$(".port").animate({opacity:.3},2500).animate({fontSize:"60px"}).animate({letterSpacing:"10px"});
+
+$(".skillsp").animate({fontSize:"30px"},1500).animate({letterSpacing:"2px"}).animate({opacity:.6},1000);
 });
+
+
 
 
