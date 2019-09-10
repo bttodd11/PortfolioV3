b6af2750dd1f224b79d4bef73f354537514073e0
bttodd11 <bttodd@icloud.com>
2019-09-10 08:18:51 -0500

    Updated Revision with Quotes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 // Onload fade for the button
-$(document).ready(function(){
-$(".buttonLarge").fadeIn(1500)
-
-
-
-});
+$(document).ready(function () {
+    $(".buttonLarge").fadeIn(1500)
+})
 
 
 
 
+// Calling quotes from an API
+$.getJSON('https://api.quotable.io/random', function (data) {
+    console.log(`${data.content} —${data.author}`)
+    document.getElementById('quote').innerHTML = data.content + '<br>' + data.author
+})
 
 
 
