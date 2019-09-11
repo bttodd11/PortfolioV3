3d888adb70eee1319c64270d455965f1d188c8bf
bttodd11 <bttodd@icloud.com>
2019-09-11 07:59:08 -0500

    Changes made to CSS and MainJS

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-$(".port").animate({opacity:.3},2500).animate({fontSize:"60px"}).animate({letterSpacing:"10px"});
+$(".port").animate({opacity:.2},2500).animate({fontSize:"60px"}).animate({letterSpacing:"10px"});
 $(".skillsp").animate({fontSize:"30px"},1500).animate({letterSpacing:"5px"}).animate({opacity:.7},1000);
 });
 
