9d3cd924cf20043c711a0ee032c2fcd2ef680e7b
bttodd11 <bttodd@icloud.com>
2019-10-15 07:50:24 -0500

    Edited the breakpoints

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,6 +1,6 @@
 
 var i = 0;
-var txt = 'My name is Brian Todd(aka B.Todd), I am a software developer from Austin,TX. I\'ve built  websites, web applications and also software. I enjoy learning' +
+var txt = 'My name is Brian Todd(aka B.Todd), I am a software developer from Austin,TX. I\'ve built  websites and also web applications. I enjoy learning' +
 ' new technologies and also finding solutions to complex problems'
 var speed = 15;
 
